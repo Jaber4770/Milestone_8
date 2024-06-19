9518f8c3105501f60ea2d381547d8f06f090e603
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-06-19 15:20:32 +0300

    milestone 8 started

---
+++ react-with-tailwind-and-daisy/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-5xl'>Avatar</h1>
+      <div className="avatar">
+        <div className="w-32 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-20 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-16 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-8 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
